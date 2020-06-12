a251291944449ac774ff010fe23ed3be173efd2f
Takuma <takumab@gmail.com>
2020-06-12 09:33:04 +0100

    Initial commit

---
+++ test/hello-world.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('Hello world', () => {
+    it('should display hello world', (done) => {
+        request(app)
+            .get('/hello-world')
+            .expect('Content-Type', /html/)
+            .then((response) => {
+                expect(response.text).toEqual("Hello, world!")
+                done()
+            })
+            .catch(done)
+    });
+});
