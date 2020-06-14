52a159ae3c8de79b1d0f0c7ae4284506e9e97ba3
Takuma <takumab@gmail.com>
2020-06-14 10:49:01 +0100

    Fix failing build

--- src/routes/hello-world.js
+++ src/routes/hello-world.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.send('Hello, !');
+    res.send('Hello, world!');
 });
 
 module.exports = router;
