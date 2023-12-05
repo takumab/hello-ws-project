24a226bc9f05cbfc3748e6de9f1c41452b7ee7d8
tacuma.bellford <tacuma.bellford@lego.com>
2023-12-05 10:33:29 +0000

    Add action to test file system ops

---
+++ .github/actions/testing-workflows.js
@@ -0,0 +1,10 @@
+import path from "path";
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+console.log(`Directory name: ${__dirname}`)
+
+const outputPath = path.join(__dirname,'..', '..', 'workspaces.yml')
+console.log({ outputPath });
