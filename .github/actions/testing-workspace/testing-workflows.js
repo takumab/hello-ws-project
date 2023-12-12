35496e870b21a607cfd718475a407a482edf0b71
tacuma.bellford <tacuma.bellford@lego.com>
2023-12-12 11:38:59 +0000

    Add status and check git diff staged

--- .github/actions/testing-workspace/testing-workflows.js
+++ .github/actions/testing-workspace/testing-workflows.js
@@ -8,5 +8,5 @@ const __dirname = dirname(__filename)
 console.log(`Directory name: ${__dirname}`)
 
 const outputPath = join(__dirname,'..', '..', 'workspaces.yml')
-writeFileSync(outputPath, '@hello-tacKumabAC');
+writeFileSync(outputPath, '@hello-tacKumabAChellooo');
 console.log({ outputPath });
